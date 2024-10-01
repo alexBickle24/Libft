75e88750d56747867edf7288471e724a15863ffa
alex <alexcarrillomc@outlook.com>
2024-10-01 09:13:24 +0200

    subo desde casa

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -3,23 +3,36 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strtrim.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: alcarril <alcarril@student.42.fr>          +#+  +:+       +#+        */
+/*   By: alex <alex@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/30 14:04:17 by alcarril          #+#    #+#             */
-/*   Updated: 2024/09/30 19:20:28 by alcarril         ###   ########.fr       */
+/*   Updated: 2024/09/30 23:56:35 by alex             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-
+// llevandotelo hasta el finalo hasta el principio
 char	*ft_strtrim(char const *s1, char const *set)
 {
-	size_t	s1_len;
-	size_t	i;
+	size_t	len;
+	int	i;
+	char *ptr;
 	
-	i = 0;
-	s1_len = ft_strlen(s1);
-	while (*s1 && (*s1 != set[i]))
+	i = -1;
+	len = ft_strlen(s1) + 1;
+	while (s1[len - len] != set[i++])
 	{
-		i++;
+		if(i-1)
+			len++;
 	}
+	while(s1[len--])
+	{
+		if()
+			len--;	
+	}
+	ptr = (char *)malloc()
+	if (!(ptr))
+		return(0);
+	
 }
+//la otra opciones hacer un substring dadole los parametros
+//el len y el size los calcularia con los bucles
