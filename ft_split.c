cdc884282c6fffe7c935ade05a66cfd1103679ea
alex <acarril42@student.42madrid.com>
2024-10-08 21:15:07 +0200

    subo split pero tego que comprbar cosas

--- ft_split.c
+++ ft_split.c
@@ -6,47 +6,95 @@
 /*   By: alcarril <alcarril@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/10/02 20:22:17 by alex              #+#    #+#             */
-/*   Updated: 2024/10/08 19:19:12 by alcarril         ###   ########.fr       */
+/*   Updated: 2024/10/08 21:13:16 by alcarril         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
-/*
+
+char	*ft_counter_begin(char *s, char c)
+{
+	size_t	i;
+	size_t	j;
+	char	*aux_b;
+
+	aux_b = (((ft_strlen(s) / 2) + 1), 1);
+	while (s[i] != '\0')
+	{
+		if ((i == 0 && s[i] != c) || (s[i] == c && s[i - 1] != c))
+		{
+			aux_b[j] = i;
+			j++;
+		}
+		i++;
+	}
+	return (aux_b);
+}
+
+char	*ft_counter_end(char *s, char c)
+{
+	size_t	i;
+	size_t	t;
+	char	*aux_e;
+
+	aux_e = (((ft_strlen(s) / 2) + 1), 1);
+	while (s[i] != '\0')
+	{
+		if ((s[i] == c && s[i + 1] == '\0') || (s[i] == c && s[i - 1] != c))
+		{
+			aux_e[t] = i;
+			t++;
+		}
+		i++;
+	}
+	return (aux_e);
+}
+
+void	*ft_free(char **ptr, int i)
+{
+	while (i >= 0)
+	{
+		free((ptr + i));
+		i--;
+	}
+	free(ptr);
+	return (NULL);
+}
+
 char	**ft_split(char const *s, char c)
 {
+	size_t	n;
 	char	**ptr;
-	void	*aux_p;
-	void	*aux_e;
-	int	i;
-	int	j;
-	int	t;
-	int	n;
+	char	*aux_b;
+	char	*aux_e;
 
 	n = 0;
-	i = -1;
-	j = -1;
-	t = -1;
-	aux_p = ft_calloc(((ft_strlen(s) / 2) + 1), 1);//compribar i calloc retorna null
-	aux_e = ft_calloc(((ft_strlen(s) / 2) + 1), 1);//lo mismo i calloc
-	while (s[i++])//la creacion de lso dos arrays e basa en que todo lo que tenga principio tenga final por ell pouede hacer correspondencia entre aux_p y aux_e
-	{
-		if ((i = 0 && s[i] != c) || (s[i] == c && s[i-1] != c))
-			*(int *)&aux_p[j++] = i; 
-		if ((s[i] == c && s[i+1] == '\0') || (s[i] == c && s[i-1] != c))
-			*(int *)&aux_e[t++] = i;
-	}
-	ptr = (char **)malloc(sizeof(char *) * (j + 1));//funcion que elimine esas reservas de memoria 
+	aux_b = ft_counter_begin(s, c);
+	aux_e = ft_counter_end(s, c);
+	ptr = (char **)malloc(ft_strlen(aux_b) * sizeoff(char *));
 	if (!ptr)
-		return(ft_free (ptr), NULL);//
-	while (n < (j + 1))//si el malloc no dejase hacer overflow la notacion cambiaria, relleno primero por el principio pero lo aux los recorro de atras alante
+		return (NULL);
+	while (n < ft_strlen(aux_b))
 	{
-		*ptr = ft_substr(s, *(int *)&aux_p[j], (*(int *)&aux_e[t] - *(int *)&aux_p[j] + 1));
-		ptr++;
+		*(ptr + n) = ft_substr(s, aux_b[n], (aux_e[n] - aux_b[n] + 1));
+		if (!*(ptr + n))
+			return (ft_free(ptr, n));
 		n++;
-		j--;
-		t--;
 	}
-	return(ptr);
+	return (ptr);
 }
-//me sobran 10 lineas.
+/*
+int	main(void)
+{
+	char	**ptr;
+	char	*s1;
+	char	c;
+
+	s1 = "comeme los huevos";
+	c = ' ';
+	ptr = ft_split(s1, c);
+	printf("%s", *(ptr + 1));
+	free(ptr);
+	return (0);
+}
 */
